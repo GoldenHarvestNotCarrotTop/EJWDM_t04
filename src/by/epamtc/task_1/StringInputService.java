4748f63383254970455580e717178ffec7d35597
NotaLonelyDay <kalbasa2002@gmail.com>
2021-04-27 10:00:02 +0300

    Added files logic

---
+++ src/by/epamtc/task_1/StringInputService.java
@@ -0,0 +1,27 @@
+package by.epamtc.task_1;
+
+import by.epamtc.task_1.exception.WrongFileException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class StringInputService {
+    public static String readTextFromStdin(){
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
+    }
+
+    public static String readTextFromFile(String filename) throws WrongFileException {
+
+
+        File file = new File(filename);
+
+        try(Scanner scanner = new Scanner(file)){
+            return scanner.nextLine();
+        } catch (FileNotFoundException e){
+            throw new WrongFileException("Can't read file");
+        }
+    }
+}
